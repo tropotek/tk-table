be14e167e54734227a68bd181a3e77c9c1928c0a
mifsudm <mifsudm@unimelb.edu.au>
2017-10-04 14:38:21 +1100

    Minor Code Updates - 252s-dev

---
+++ Tk/Table/Cell/ArrayObject.php
@@ -0,0 +1,23 @@
+<?php
+namespace Tk\Table\Cell;
+
+
+/**
+ * @author Michael Mifsud <info@tropotek.com>
+ * @link http://www.tropotek.com/
+ * @license Copyright 2015 Michael Mifsud
+ */
+class ArrayObject extends Text
+{
+
+
+    public function getPropertyValue($obj, $property)
+    {
+        $value = $this->getObjectPropertyValue($obj, $property);
+        if (is_array($value)) {
+            $value = implode(', ', $value);
+        }
+        return $value;
+    }
+
+}
