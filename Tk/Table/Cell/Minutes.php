ddae7b5aaf81826e216ab443537b08d8b3c9c614
mifsudm <mifsudm@unimelb.edu.au>
2019-09-24 10:32:43 +1000

    Updated dialogs and fixed Minutes cell

--- Tk/Table/Cell/Minutes.php
+++ Tk/Table/Cell/Minutes.php
@@ -12,8 +12,33 @@ class Minutes extends Text
 
     public function getPropertyValue($obj)
     {
-        $value = $this->getRawValue($obj);
-        $value = gmdate("G:i", (int)$value*60);
+        $value = self::min2Str($this->getRawValue($obj));
         return $value;
     }
+
+    /**
+     * Convert minutes to a `h:m` string
+     *
+     * @param int $minutes
+     * @return string
+     */
+    public static function min2Str($minutes)
+    {
+        $h = floor($minutes / 60);
+        $m = $minutes - ($h * 60);
+        return sprintf('%02d:%02d', $h, $m);
+    }
+
+    /**
+     * Convert a string in the format of `h:m` to minutes
+     *
+     * @param $str
+     * @return int
+     */
+    public static function str2min($str)
+    {
+        list($h, $m) = explode(':', $str);
+        return (int)(((int)$h*60)+(int)$m);
+    }
+
 }
