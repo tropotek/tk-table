040f768fd7d3e3c78f6af9ffc2161e2e202bac63
tropotek <info@tropotek.com>
2024-01-12 13:23:02 +1100

    move template paths

--- config/20-config.php
+++ config/20-config.php
@@ -3,5 +3,5 @@ use Tk\Config;
 
 return function (Config $config)
 {
-    $config->set('path.template.table', '/vendor/ttek/tk-table/templates/Table.html');
+    $config->set('path.template.table', '/vendor/ttek/tk-table/templates/dom/bootstrap5/table.html');
 };
