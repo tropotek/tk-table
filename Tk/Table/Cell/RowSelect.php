0d90bba585308bf78f0c9478832cd88a5d8644d2
tropotek <info@tropotek.com>
2025-12-22 13:42:20 +1100

    Fix table row select header when no rows

--- Tk/Table/Cell/RowSelect.php
+++ Tk/Table/Cell/RowSelect.php
@@ -31,16 +31,21 @@ class RowSelect extends Cell
         return $row->{$this->getProperty()} ?? '';
     }
 
+    public function getHeader(): string
+    {
+        //$header = parent::getHeader();
+        $disabled = $this->isDisabled() ? 'disabled' : '';
+        return sprintf('<input type="checkbox" name="%s_all" title="Select All" class="tk-tcb-head" %s />', $this->getName(), $disabled);
+    }
+
     public function getHtml(): string
     {
         $row = $this->getRow();
         if (is_null($row)) return '';
         if (is_array($row)) $row = (object)$row;
 
-        $disabled = $this->isDisabled() ? 'disabled' : '';
-        $this->setHeader(sprintf('<input type="checkbox" name="%s_all" title="Select All" class="tk-tcb-head" %s />', $this->getName(), $disabled));
-
         $id = $this->getValue();
+        $disabled = $this->isDisabled() ? 'disabled' : '';
         return sprintf('<input type="checkbox" name="%s[]" value="%s" class="tk-tcb" %s/>', $this->getName(), e($id), $disabled);
     }
 
